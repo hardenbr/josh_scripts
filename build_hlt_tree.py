d64d2ff46ce739cea885fa6fa887eaf982127934
Joshua Robert Hardenbrook <hardenbr@lxplus416.cern.ch>
2013-09-12 19:17:16 +0200

    added scripts for analyzing and submitting hlt tree analysis from new openhlt producer files

---
+++ build_hlt_tree.py
@@ -0,0 +1,175 @@
+from  optparse  import OptionParser
+import ROOT as rt
+from DataFormats.FWLite import Events, Handle
+import os, sys
+
+parser = OptionParser()
+
+default_file = "/afs/cern.ch/work/h/hardenbr/2013/HIGGS_DIPHOTON_HLT/fscan_mass100_testfile/res/temp_27_19.root"
+
+parser.add_option("-f", "--file", dest="filename",
+                  help="hlt.root file name to analyze FILE",default=default_file,
+                  action="store",type="string")
+
+parser.add_option("-o", "--outfile", dest="outfilename",
+                  help="tree.root file name to output",default="hlt_tree.root",
+                  action="store",type="string")
+
+parser.add_option("-t", "--tag", dest="tag",
+                  help="which HLT Process",
+                  action="store",type="string", default="HLT3")
+
+(options, args) = parser.parse_args()
+
+parser.print_help()
+
+HLT_TAG = "HLT3"
+
+###WRITE OUT ALL OF THE HANDLE INFORMATION###
+cluster = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltActivityPhotonClusterShape"   ,"",                HLT_TAG)
+
+ecal_iso = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltActivityPhotonEcalIso"   ,"",                HLT_TAG)
+
+hoe = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltActivityPhotonHcalForHE"   ,"",                HLT_TAG)
+
+hcal_iso = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltActivityPhotonHcalIso"   ,"",                HLT_TAG)
+
+track_iso = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltActivityPhotonHollowTrackIsoWithId"   ,"",                HLT_TAG)
+
+r9 = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltActivityR9ID"           ,"",                HLT_TAG)
+
+###SEEDED INFORMATION###
+cluster_seed = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltL1SeededHLTClusterShape","", HLT_TAG)
+
+ecal_iso_seed = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltL1SeededPhotonEcalIso"   ,"",                HLT_TAG)
+
+hoe_seed = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltL1SeededPhotonHcalForHE"   ,"",                HLT_TAG)
+
+hcal_iso_seed = ("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltL1SeededPhotonHcalIso"   ,"",                HLT_TAG)
+
+r9_seed =("edm::AssociationMap<edm::OneToValue<vector<reco::RecoEcalCandidate>,float,unsigned int> >"    ,"hltL1SeededR9ID"           ,"",                HLT_TAG)
+
+
+##SUMMARY
+seed = (cluster_seed, ecal_iso_seed, hoe_seed, hcal_iso_seed, r9_seed)
+non_seed = (cluster, ecal_iso, hoe, hcal_iso, track_iso, r9)
+handles= []
+
+#build the vectors
+
+#fill the handle array (label, handle)
+for ii in seed+non_seed:
+    handle = Handle(ii[0])
+    label = ii[1]
+    middle = ii[2]
+    process = ii[3]
+
+    handles.append([(label,middle,process),handle])
+        
+rt.gROOT.SetBatch()
+counter = 0
+
+#build the vectors for the tree
+vectors = []
+vectors_cand = []
+for ii in handles:
+    vector = rt.vector("float")()
+    vectors.append((ii[0][0],vector))
+
+candidates = ["pt","eta","phi","energy"]
+
+for ii in candidates:
+    vector = rt.vector("float")()
+    vectors_cand.append((ii,vector))
+
+events = Events(options.filename)
+tree = rt.TTree("hltTree", "hltTree")
+
+#set the branches in the tree
+for ii in vectors:
+    tree.Branch(ii[0],ii[1])
+
+for ii in vectors_cand:
+    tree.Branch(ii[0],ii[1])
+
+
+# Create a struct
+rt.gROOT.ProcessLine(\
+      "struct MyStruct{\
+      Int_t run;\
+      Int_t ls;\
+      Int_t event;\
+      };")
+
+from ROOT import MyStruct
+
+# Create branches in the tree
+s = MyStruct()
+
+tree.Branch("run",rt.AddressOf(s,"run"),"run/I")
+tree.Branch("ls",rt.AddressOf(s,"ls"),"ls/I")
+tree.Branch("event",rt.AddressOf(s,"event"),"event/I")
+
+#retreives the lables for a handle for an event
+def getAllLabels(event):
+    for ii in handles:
+        input_tag = ii[0]
+        handle = ii[1]
+        event.getByLabel(input_tag, handle)
+
+#gets the maps for all of the handles and returns a list
+def getMaps():
+    maps = []
+
+    for ii in handles:
+        product = ii[1].product()
+        name = ii[0][0]        
+        maps.append((name,ii[1].product()))
+
+    return maps
+
+#get the events from the file 
+for event in events:    
+    if counter % 1000 == 0: print "Analyzing " + str(counter) + "..."
+
+    getAllLabels(event)
+    maps = getMaps()
+
+    aux = event.eventAuxiliary()
+    s.run = aux.run()
+    s.ls = aux.luminosityBlock()
+    s.event = aux.event()
+    
+    for map in maps:
+        name = map[0]
+        values = map[1].values()
+        cand = map[1].refProd().key.product()
+        handle_ii = maps.index(map)
+        
+        for ii in range(values.size()):
+            vectors[handle_ii][1].push_back(values[ii])
+
+        for ii in range(cand.size()):
+            for vec in vectors_cand:
+                name = vec[0]
+                val = eval("cand[ii].%s()"%name)                
+                if maps.index(map) == 0: vec[1].push_back(val)                              
+
+    #fill tree and clear the vectors out
+    tree.Fill()
+    
+    for ii in vectors:
+        ii[1].clear()
+
+    for ii in vectors_cand:
+        ii[1].clear()
+                        
+    counter+=1
+    continue
+
+outfile = rt.TFile(options.outfilename,"RECREATE")
+tree.CloneTree().Write()
+outfile.Close()
+
+
+
---
+++ hlt_analyzer_nhlt_tree_bsub.py
@@ -0,0 +1,144 @@
+##Generates config files, and batch submission batch.src files with appropriate directories
+from  optparse  import OptionParser
+import sys, os
+
+parser = OptionParser()
+
+parser.add_option("-r", "--raw", dest="rawlist",
+		                    help="list of raw files to analyze",
+		                    action="store",type="string")
+
+parser.add_option("-c","--config",dest="config",
+		                    help="hlt config modified to have replacements done for raw list",
+		                    action="store",type="string")
+
+parser.add_option("-o", "--output", dest="output",
+		                    help="output destination",
+		                    action="store",type="string")
+
+parser.add_option("-j", "--jobs", dest="jobs",
+		                    help="number of jobs to run at once",
+		                    action="store",type="string")
+
+parser.add_option("-p", "--rmprod", dest="rmprod",
+		                    help="remove the producer files generated",
+		                    action="store_true",default=False)
+
+
+(options, args) = parser.parse_args()
+
+parser.print_help()
+
+#get some names and paths
+pwd = os.getenv("PWD")
+output_dir = options.output
+cfg_name = options.config
+
+#OPEN THE FILES
+raw = open(options.rawlist,"r")
+cfg = open(cfg_name,"r")
+#READ THE LINES IN
+#reco_lines = reco.readlines()
+raw_lines = raw.readlines()
+
+#Count how many config files we will need
+#there better not be extra lines in the lists!
+#n_reco = len(reco_lines)
+n_raw = len(raw_lines)
+
+#make the raw list we will be inserting
+raw_list = ""
+for ii in raw_lines:
+        raw_list += "'"+ii.rstrip("\n")+"',\n"
+
+raw_list=raw_list.rstrip(",\n")+"\n"
+
+#remove old instances of the output directory
+os.system("rm -r " + output_dir)
+#build the directories
+os.system("mkdir " + output_dir)
+os.system("mkdir " + output_dir+"/cfgs")
+os.system("mkdir " + output_dir+"/logs")
+os.system("mkdir " + output_dir+"/src")
+os.system("mkdir " + output_dir+"/res")
+
+#strings to be replaced in the config file
+#reco_replace_string = "#$REPLACE_RECO$#"
+raw_replace_string = "#$REPLACE_RAW$#"
+output_replace_string = "#$REPLACE_OUTPUT$#"
+
+commands = []
+
+
+for ii in range(n_raw):
+	#make the new cfg file	
+        new_file_name = output_dir+"/cfgs/"+cfg_name.rstrip(".py")+"_"+str(ii)+".py"
+        cfg_file = open(cfg_name, "r")
+        cfg_file_lines = cfg_file.readlines()
+        new_file = open(new_file_name,"a")
+	output_file_path = output_dir+"/res/ohlt_output_"+str(ii)+".root" 
+	
+        #read all the lines of the old cfg file and replace the replace text
+        for jj in range(len(cfg_file_lines)):
+		#dont replace the reco for now (version is too old 53X)
+                #if reco_replace_string in cfg_file_lines[jj]:
+                #        cfg_file_lines[jj] = "'"+reco_lines[ii].rstrip("\n")+"'"
+                if raw_replace_string in cfg_file_lines[jj]:
+                        cfg_file_lines[jj] = cfg_file_lines[jj].replace(raw_replace_string,raw_lines[ii].rstrip("\n"))
+                if output_replace_string in cfg_file_lines[jj]:
+                        replace_string_out= output_file_path
+                        cfg_file_lines[jj] = cfg_file_lines[jj].replace(output_replace_string,replace_string_out)
+        #write the cfg file
+        new_file.writelines(cfg_file_lines)
+
+	#generate the commands to run on the raw and convert to trees
+	output_tree_path = output_dir+"/res/hlt_tree_%i.root" % ii
+	cmd_cmsrun = "cmsRun %s\n" % new_file_name
+
+	cmd_tree = "~/josh_scripts/build_hlt_tree.py -f %s -o %s \n" % (output_file_path,output_tree_path)
+	cmd_rm = "rm %s \n" % output_file_path
+
+	commands.append((cmd_cmsrun,cmd_tree,cmd_rm))
+
+njobs = options.jobs
+job = 0
+file_counter = 0
+cmds_per_job = int(len(commands)) / int(njobs) #integer division
+bsub_cmds = []
+bsub_file = None
+
+#loop over all commands
+for ii in range(len(commands)):
+	#make sure the extra jobs are added to the last file
+	if (file_counter == 0) and (job != njobs):		
+		#write a new job
+		bsub_file_name = output_dir+"/src/bsub_%i.src" % job
+		bsub_file = open(bsub_file_name,"a")
+		bsub_file.write('#!/bin/bash\n')
+		bsub_file.write("cd /afs/cern.ch/user/h/hardenbr/2013/HIGGS_DIPHOTON_HLT/NEW_OPEN_HLT/CMSSW_5_2_8_patch1/src\n")
+		bsub_file.write("export SCRAM_ARCH=slc5_amd64_gcc462 \n")
+		bsub_file.write("eval `scramv1 ru -sh`\n")
+
+		cmd="bsub -q 1nd "
+		cmd+= "-o %s/logs/job_%i.log " % (output_dir ,job)
+		cmd+="source "+ bsub_file_name
+
+		bsub_cmds.append(cmd)
+
+		#increment the counter
+		job+=1
+
+	bsub_file.write(commands[ii][0])
+	bsub_file.write(commands[ii][1])
+	if options.rmprod:
+		bsub_file.write(commands[ii][2])
+	
+	#if we are at the number of commands per job reset the counter
+	file_counter+=1
+	if file_counter == cmds_per_job:file_counter = 0		
+	if job == njobs:
+		continue
+
+#print out the submission commands
+for ii in bsub_cmds:print ii
+
